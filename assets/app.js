fa489879064ab04efff03379077e125c1cd8c270
Anatolii <74195955+anatolii-semochko@users.noreply.github.com>
2025-08-03 17:02:43 +0300

    Add Wallet functionality and Fix configuration

--- assets/app.js
+++ assets/app.js
@@ -6,12 +6,14 @@ import '@css/custom.scss'
 import '@js/help-link'
 import React from 'react'
 import store from '@react/store/index'
+import { Buffer } from 'buffer'
 import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import CreateMaterial from '@react/components/create-material/CreateMaterial'
 import CheckToken from '@react/components/check-token/CheckToken'
 
 window.bootstrap = bootstrap
+window.Buffer = Buffer
 
 const createMaterial = document.getElementById('create-material')
 if (createMaterial) {
