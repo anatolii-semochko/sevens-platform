698159f54f8190dc68de135dba114f2c0918d01b
anatolii <anatoliy.semochko@gmail.com>
2025-09-25 23:41:40 +0300

    Add token form fields max size from IDL

--- assets/react/components/create-token-material/components/token/TokenForm.js
+++ assets/react/components/create-token-material/components/token/TokenForm.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { sevensIdl } from '@js/blockchain/sevens-token'
 import { Input, TextArea, Select } from '@react/components/form-elements/Inputs'
 
-const Name = ({name, setName, nameMaxLength, error, setErrorMessage}) => (
+const Name = ({name, setName, maxLength, error, setErrorMessage}) => (
     <div className="col-12 col-lg-4">
         <label htmlFor="tokenName" className="form-label">Token name:</label>
         <Input
             id={'tokenName'}
             placeholder={'required'}
-            maxLength={nameMaxLength}
+            maxLength={maxLength}
             required={true}
             value={name}
             onChange={setName}
@@ -18,13 +18,13 @@ const Name = ({name, setName, nameMaxLength, error, setErrorMessage}) => (
     </div>
 )
 
-const Author = ({author, setAuthor, authorMaxLength, error, setErrorMessage}) => (
+const Author = ({author, setAuthor, maxLength, error, setErrorMessage}) => (
     <div className="col-12 col-lg-4">
         <label htmlFor="tokenAuthor" className="form-label">Token author:</label>
         <Input
             id={'tokenAuthor'}
             placeholder={'optional'}
-            maxLength={authorMaxLength}
+            maxLength={maxLength}
             required={true}
             value={author}
             onChange={setAuthor}
@@ -55,13 +55,13 @@ const Burnable = ({burnable, setBurnable, error, setErrorMessage}) => (
     </div>
 )
 
-const Description = ({description, setDescription, descriptionMaxLength, error, setErrorMessage}) => (
+const Description = ({description, setDescription, maxLength, error, setErrorMessage}) => (
     <div className="col-12">
         <label htmlFor="tokenDescription" className="form-label">Token description:</label>
         <TextArea
             id={'tokenDescription'}
             placeholder={'optional'}
-            maxLength={descriptionMaxLength}
+            maxLength={maxLength}
             rows={3}
             value={description}
             onChange={setDescription}
@@ -76,12 +76,17 @@ export const TokenForm = ({tokenData, setTokenData, setErrorMessage}) => {
     const [author, setAuthor] = useState('')
     const [burnable, setBurnable] = useState(false)
     const [description, setDescription] = useState('')
+    const [lengths, setLengths] = useState({})
 
-    // TODO - take values from sevens IDL
-    // console.log({sevensIdl})
-    const nameMaxLength = 32
-    const authorMaxLength = 32
-    const descriptionMaxLength = 128
+    useEffect(() => {
+        const lengthConstants = {}
+        sevensIdl.constants.map(constant => {
+            if (constant.name === 'MAX_TOKEN_NAME_LENGTH') lengthConstants.name = constant.value
+            if (constant.name === 'MAX_AUTHOR_LENGTH') lengthConstants.author = constant.value
+            if (constant.name === 'MAX_DESCRIPTION_LENGTH') lengthConstants.description = constant.value
+        })
+        setLengths(lengthConstants)
+    }, [])
 
     useEffect(() => {
         setName(tokenData?.name || '')
@@ -97,12 +102,12 @@ export const TokenForm = ({tokenData, setTokenData, setErrorMessage}) => {
     return (
         <div className="mb-3">
             <div className="row g-3 mb-3">
-                <Name {...{name, setName, nameMaxLength, setErrorMessage}} />
-                <Author {...{author, setAuthor, authorMaxLength, setErrorMessage}} />
+                <Name {...{name, setName, maxLength: lengths.name, setErrorMessage}} />
+                <Author {...{author, setAuthor, maxLength: lengths.author, setErrorMessage}} />
                 <Burnable {...{burnable, setBurnable, setErrorMessage}} />
             </div>
             <div className="row g-3">
-                <Description {...{description, setDescription, descriptionMaxLength, setErrorMessage}} />
+                <Description {...{description, setDescription, maxLength: lengths.description, setErrorMessage}} />
             </div>
         </div>
     )
